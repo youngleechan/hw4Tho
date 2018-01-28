b9a09fd4d2cf6ed70f52b0b567cb8fa8b7304932
Younglee <younglee@Younglees-iMac.local>
2018-01-28 14:12:06 +0200

    Initial Commit

---
+++ homeWork4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homeWork4
+//
+//  Created by Younglee on 1/28/18.
+//  Copyright © 2018 Younglee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
